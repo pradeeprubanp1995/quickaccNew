884a52f8bc406cffac42732f93b2995e80e3e148
muthukumarancolan <muthukumaran.r@colanonline.com>
2019-01-08 18:07:39 +0530

    first try

--- index.php
+++ index.php
@@ -13,6 +13,6 @@
  */
 define('WP_USE_THEMES', true);
 echo"Hi";
-print_r('ruban');
+print_r('hai');
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
