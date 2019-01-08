c2265b31f58d756e4da114878b972f83690d383f
muthukumarancolan <muthukumaran.r@colanonline.com>
2019-01-08 20:17:42 +0530

    MUT Result table Created

---
+++ database/migrations/2019_01_08_142750_create_results_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('dept_id');
+            $table->date('today_date');
+            $table->string('user_answer');
+            $table->integer('points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
