c64dbf431dd7c9ae3d685e2f155df3dab30f26d4
pradeep7373 <pradeepruban@colanonline.com>
2019-01-11 20:07:09 +0530

    ruba profileview

--- resources/views/userprofileview.blade.php
+++ resources/views/userprofileview.blade.php
@@ -17,7 +17,7 @@
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">UserID</label>
                           <div class="col-sm-9">
-                            <?php echo $data['user_id'];?>
+                            <?php echo $data[0]['user_id'];?>
                             
                           </div>
                         </div>
@@ -25,7 +25,7 @@
                       <div class="col-md-6">
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Name</label>
-                          <div class="col-sm-9"> <?php echo $data['name'];?>
+                          <div class="col-sm-9"> <?php echo $data[0]['name'];?>
                           </div>
                         </div>
                       </div>
@@ -35,7 +35,7 @@
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Gender</label>
                           <div class="col-sm-9">
-                            <?php echo $data['gender'];?>
+                            <?php echo $data[0]['gender'];?>
                           </div>
                         </div>
                       </div>
@@ -43,7 +43,7 @@
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Department</label>
                           <div class="col-sm-9">
-                        {{$dept->dept_name}}
+                        {{$data[1]->dept_name}}
                           </div>
                         </div>
                       </div>
@@ -53,7 +53,7 @@
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Email Address</label>
                           <div class="col-sm-9">
-                           <?php echo $data['email'];?>
+                           <?php echo $data[0]['email'];?>
                           </div>
                         </div>
                       </div>
@@ -61,7 +61,7 @@
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Image</label>
                           <div class="col-sm-9">
-                             <img src="{{URL('/')}}/uploads/{{$data['images']}}" width="100px" height="100px"/></img>
+                             <img src="{{URL('/')}}/uploads/{{$data[0]['images']}}" width="100px" height="100px"/></img>
                             
                           </div>
                         </div>
